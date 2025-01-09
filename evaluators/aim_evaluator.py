357604cf588e2d7a7e9c1ba6619d984e24d4755f
Nathen <nathenbr@gmail.com>
2025-01-09 18:18:40 -0500

    Initial root finding stuff

--- evaluators/aim_evaluator.py
+++ evaluators/aim_evaluator.py
@@ -1,12 +1,13 @@
+import math
+
 from evaluators import preprocessing
 from evaluators.preprocessing import OsuDifficultyHitObject
 
-
-def evaluate_aim_difficulties(curr_y_values, prev_y_values, next_y_values, prev_curr_y_values, curr_next_y_values):
+def evaluate_aim_difficulties(curr_y_values, prev_y_values, next_y_values, prev_curr_y_values, curr_next_y_values, distance_bonus_multiplier):
     aim_difficulties = []
 
     for current in preprocessing.OsuDifficultyHitObject.objects:
-        aim_difficulties.append(distance_difficulties(current) + angle_bonus(current, curr_y_values, prev_y_values, next_y_values, prev_curr_y_values, curr_next_y_values))
+        aim_difficulties.append(distance_difficulties(current) * distance_bonus_multiplier + angle_bonus(current, curr_y_values, prev_y_values, next_y_values, prev_curr_y_values, curr_next_y_values))
 
     return aim_difficulties
 
@@ -24,44 +25,43 @@ def angle_bonus(current: OsuDifficultyHitObject, curr_y_values, prev_y_values, n
     if current.index < 3 or current.index == len(preprocessing.OsuDifficultyHitObject.objects) - 2:
         return 0
 
-    curr_angle = current.angle
-    prev_angle = current.previous(0).angle
-    next_angle = current.next(0).angle
+    curr_angle = abs(current.angle) * 180 / math.pi
+    prev_angle = abs(current.previous(0).angle) * 180 / math.pi
+    next_angle = abs(current.next(0).angle) * 180 / math.pi
 
     curr_velocity = current.distance / current.strain_time
     prev_0_velocity = current.previous(0).distance / current.previous(0).strain_time
     prev_1_velocity = current.previous(1).distance / current.previous(1).strain_time
     next_velocity = current.next(0).distance / current.next(0).strain_time
 
-    x_values = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180]
+    x_values = [0, 30, 60, 90, 120, 150, 180]
 
     curr_angle_bonus = lerp(curr_angle, x_values, curr_y_values) * min(curr_velocity, prev_0_velocity)
     prev_angle_bonus = lerp(prev_angle, x_values, prev_y_values) * min(prev_0_velocity, prev_1_velocity)
     next_angle_bonus = lerp(next_angle, x_values, next_y_values) * min(next_velocity, curr_velocity)
 
-    x_values_2 = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]
+    x_values_2 = [0, 60, 120, 180, 240, 300, 360]
 
-    prev_curr_angle_bonus = lerp(curr_angle, x_values_2, prev_curr_y_values) * min(curr_velocity, prev_0_velocity, prev_1_velocity)
-    curr_next_angle_bonus = lerp(prev_angle, x_values_2, curr_next_y_values) * min(next_velocity, curr_velocity, prev_0_velocity)
+    prev_curr_angle_bonus = lerp(curr_angle + prev_angle, x_values_2, prev_curr_y_values) * min(curr_velocity, prev_0_velocity, prev_1_velocity)
+    curr_next_angle_bonus = lerp(next_angle + curr_angle, x_values_2, curr_next_y_values) * min(next_velocity, curr_velocity, prev_0_velocity)
 
     return curr_angle_bonus + prev_angle_bonus + next_angle_bonus + prev_curr_angle_bonus + curr_next_angle_bonus
-
 
 
 def lerp(x, x_values, y_values):
     if len(x_values) == 0 or len(x_values) != len(y_values):
         return 0
 
-    if x < x_values[0]:
+    if x <= x_values[0]:
         return y_values[0]
-    if x > x_values[-1]:
+    if x >= x_values[-1]:
         return y_values[-1]
 
-    i = len(x_values) - next(i for i, val in enumerate(reversed(x_values), 1) if val < x)
+    for i in range(1, len(x_values)):
+        if x_values[i] >= x:
+            break
 
-    if i < 0 or i >= len(x_values):
-        return 0
+    x0, x1 = x_values[i - 1], x_values[i]
+    y0, y1 = y_values[i - 1], y_values[i]
 
-    x_adj = (x - x_values[i]) / (x_values[i + 1] - x_values[i])
-
-    return ((y_values[i+1] - y_values[i]) * x_adj + x_values[i+1] * y_values[i] - x_values[i] * y_values[i+1]) / (x_values[i+1] - x_values[i])
+    return y0 + (y1 - y0) * (x - x0) / (x1 - x0)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 
+import scipy.optimize
 from beatmap_reader import BeatmapIO
 from osu_analysis import StdMapData
 
@@ -7,24 +9,90 @@ import evaluators.aim_evaluator
 from evaluators import preprocessing
 
 import numpy as np
+from scipy.optimize import minimize
+
+from evaluators.aim_evaluator import evaluate_aim_difficulties
+
+best_error = math.inf
+
+def objective_function(params):
+    global best_error
+
+    curr_y_values = params[:7]
+    prev_y_values = params[7:14]
+    next_y_values = params[14:21]
+    prev_curr_y_values = params[21:28]
+    curr_next_y_values = params[28:35]
+    distance_bonus_multiplier = params[35]
+
+    computed_aim_difficulties = evaluate_aim_difficulties(curr_y_values, prev_y_values, next_y_values, prev_curr_y_values, curr_next_y_values, distance_bonus_multiplier)
+
+    combined_list = list(zip(computed_aim_difficulties, deviations.average_offsets))
+
+    # Compute the squared error between computed and observed values
+    error = sum((difficulty - deviation) ** 2 for difficulty, deviation in combined_list)
+
+    if best_error > error:
+        print(f"Best error: {error}")
+
+    best_error = min(best_error, error)
+
+    return error
 
 if __name__ == '__main__':
-    print("Setting up deviations")
     beatmap = BeatmapIO.open_beatmap('./maps/Example/Adust Rain - Seven Style (Ayla) [Nightmare Essence].osu')
     map_data = StdMapData.get_map_data(beatmap)
 
+    print("Creating difficulty hit objects")
+    preprocessing.create_objects(map_data)
+
     replay_directory = './replays/Example'
 
-    preprocessing.create_objects(map_data)
-    difficulties = evaluators.aim_evaluator.evaluate_aim_difficulties([0], [0], [0], [0], [0])
+    print("Getting replay note error")
     deviations.create_deviations(map_data, replay_directory)
 
-    offset_mean = np.mean(deviations.average_offsets)
-    difficulty_mean = np.mean(difficulties)
+    # Define initial guesses for the values of curr_y_values, prev_y_values, next_y_values, etc.
+    initial_guess = np.zeros(35)
 
-    for difficulty in difficulties:
-        difficulty *= offset_mean / difficulty_mean
+    # Call minimize to find the optimal parameters
+    result = minimize(objective_function, initial_guess, method='Nelder-Mead', options={'maxiter': np.inf, 'disp': True, 'maxfun': np.inf})
 
-    zipped_deviations = list(zip(deviations.average_offsets, difficulties))
+    # Extract the optimized values
+    optimized_values = result.x
 
-    print(zipped_deviations)
+    # Extract the optimized arrays
+    multiplier = optimized_values[0]
+    optimized_curr_y_values = optimized_values[0:14]
+    optimized_prev_y_values = optimized_values[14:27]
+    optimized_next_y_values = optimized_values[27:40]
+    optimized_prev_curr_y_values = optimized_values[40:53]
+    optimized_curr_next_y_values = optimized_values[53:]
+
+    print(result)
+    print(optimized_values)
+
+# 30506.234746786547
+# [-0.29988197 -0.76875782  0.63378283  0.47458822  0.81378876 -0.59969685
+#   2.46533138  1.12500366  2.4519582  -1.29403362  0.89749442  0.03397498
+#   0.81412396  0.81913834  1.66114963 -0.84295065  0.75529879  1.15777942
+#  -0.18238538  1.43681892  1.10477889 -0.9545547  -2.91241947 -0.3948108
+#   0.07976148  0.76640801 -0.20710094  1.46424087 -1.05875852 -2.09460008
+#  -0.93851227 -0.39447279  0.74903991  0.3425476  -0.12374892]
+
+# 30506.234746782186
+# [ -1.88817953  -2.35705012   0.5617955    0.55265081  -9.7285849
+#    8.03368092   8.99620042   1.12500528   2.45194808  -1.0852154
+#    0.50954553 -15.07708046   5.5595342    4.61012454   3.63948646
+#    2.8100483   -0.77844435   1.23589769   7.89627973  -5.98769546
+#    2.22821539  -0.95455612  -2.9124321   -2.53644017   1.8122581
+#    3.47358344  -1.58163971   5.59591822  -1.44879813  -5.83393904
+#    0.11703439  -1.53650039  -3.10283546   2.86421956   3.98399441]
+
+# 30588.685580242254
+# [-0.45899294 -0.30536024  0.60736445  0.08470523  0.54932118  0.32184534
+#  -0.05300529 -1.94653474  2.3178724   0.55312054  1.29048584  0.78531118
+#   1.13306187  0.96715015  2.24279288 -0.9207931   0.45017049  1.16184665
+#   1.91484606  1.74533566  1.74308328  0.44033466 -0.2534292  -0.84916367
+#  -0.44001534  0.25872847  0.19569879  1.00216843 -0.09250267 -0.37889358
+#  -2.0125615  -2.1383017   0.25247311  2.87203951  5.06375873]
+
